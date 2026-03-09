c9777317d3ad5a611da9a7f5cf9dac9ac373b91a
Gerber Rogelio Díaz Santay <rogerdisan18@gmail.com>
2026-03-08 20:27:26 -0600

    fin proyecto

--- Program.cs
+++ Program.cs
@@ -1,17 +1,58 @@
 ﻿int j;
 int opcion;
-for (int i = 1; i <= 5; i++)
+    for (int i = 1; i <= 5; i++)
+    {
+        Console.Clear();
+        Console.WriteLine("----- MENÚ -----");
+        Console.WriteLine("1. Saludar");
+        Console.WriteLine("2. Ingresar contraseña");
+        Console.WriteLine("3. Salir");
+        Console.Write("Elija una opción: ");
+        opcion = int.Parse(Console.ReadLine());
 
-    int opcion;
+        switch (opcion)
+        {
+            case 1:
+                Console.WriteLine("Hola, bienvenido al sistema.");
+                Console.ReadLine();
+                break;
 
-for (int i = 1; i <= 5; i++)
-{
-    Console.Clear();
-    Console.WriteLine("----- MENÚ -----");
-    Console.WriteLine("1. Saludar");
-    Console.WriteLine("2. Ingresar contraseña");
-    Console.WriteLine("3. Salir");
-    Console.Write("Elija una opción: ");
-    opcion = int.Parse(Console.ReadLine());
+            case 2:
+                Console.Clear();
+                string contraseña = "1234";
+                string ingreso;
+                int intentos = 3;
 
+                for (j = 1; j <= intentos; j++)
+                {
+                    Console.Write("Ingrese la contraseña: ");
+                    ingreso = Console.ReadLine();
+
+                    if (ingreso == contraseña)
+                    {
+                        Console.WriteLine("Contraseña correcta. Acceso permitido.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Contraseña incorrecta.");
+                        Console.WriteLine("Intentos restantes: " + (intentos - j));
+                    }
+                }
+
+                Console.WriteLine("Presione Enter para volver al menú");
+                Console.ReadLine();
+                break;
+
+            case 3:
+                Console.WriteLine("Saliendo...");
+                Console.ReadLine();
+                return;
+
+            default:
+                Console.WriteLine("Opción no válida.");
+                Console.ReadLine();
+                break;
+        }
+    }
 
