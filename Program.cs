e3c7b1dcd052f5b1a84a0d00daea1ecda9f53824
Gerber Rogelio Díaz Santay <rogerdisan18@gmail.com>
2026-03-08 20:24:01 -0600

    Creando menu

--- Program.cs
+++ Program.cs
@@ -2,3 +2,16 @@
 int opcion;
 for (int i = 1; i <= 5; i++)
 
+    int opcion;
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.Clear();
+    Console.WriteLine("----- MENÚ -----");
+    Console.WriteLine("1. Saludar");
+    Console.WriteLine("2. Ingresar contraseña");
+    Console.WriteLine("3. Salir");
+    Console.Write("Elija una opción: ");
+    opcion = int.Parse(Console.ReadLine());
+
+
